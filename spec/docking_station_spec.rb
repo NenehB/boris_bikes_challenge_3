2bcf3b42ad4f32f0d59d9267fdd9ea2abd59f7b7
Patrick Barsch <PatrickGB@gmx.de>
2021-10-14 17:50:01 +0200

    19 - Using doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,9 @@
 require "docking_station.rb"
 
 describe DockingStation do
-  bike = DockingStation.new
+
   it 'DockingStation releases bike' do
-    expect(bike.respond_to?(:release_bike)).to eq(true)
+    expect(subject.respond_to?(:release_bike)).to eq(true)
   end 
 
   describe '#release_bike' do
@@ -17,30 +17,27 @@ describe DockingStation do
 
   describe '#return_bike' do
     it 'raises an error when there is already a bike' do
-      bike = Bike.new
+      bike = double(:bike)
       subject.return_bike(bike)
       expect{ DockingStation::DEFAULT_CAPACITY.times{subject.return_bike(bike) }}.to raise_error 'Bikes at maximum capacity'
     end
   end
 
   it 'Release a working bike' do
-    # station = DockingStation.new
-    bike =  Bike.new
+    bike =  double(:bike)
     subject.return_bike(bike)
     expect(bike.working?).to eq(true)
   end
 
   it 'Returns a bike to the docking station' do
-    bike = Bike.new
+    bike = double(:bike)
     subject.return_bike(bike)
     expect(subject.bikes).to eq [bike]
-    #station = DockingStation.new
-    #expect(station.return_bike(bike)).to eq(bike)
   end
 
   describe 'release_bike' do
     it 'releases a bike' do
-      bike = Bike.new
+      bike = double(:bike)
       subject.return_bike(bike)
       # we want to release the bike we docked
       expect(subject.release_bike).to eq bike
@@ -49,25 +46,25 @@ describe DockingStation do
   end
   
   it 'allows us to dock 20 bikes' do
-    expect { DockingStation::DEFAULT_CAPACITY.times{subject.return_bike(Bike.new)}}.not_to raise_error
+    expect { DockingStation::DEFAULT_CAPACITY.times{subject.return_bike(double(:bike))}}.not_to raise_error
   end
 
   it 'has a variable capacity' do
     station = DockingStation.new(30)
-    expect{30.times {station.return_bike(Bike.new)}}.not_to raise_error
+    expect{30.times {station.return_bike(double(:bike))}}.not_to raise_error
   end
    
   it 'should not release broken bikes' do
-     broken_bike = Bike.new
+     broken_bike = double(:bike)
      broken_bike.broken
      subject.return_bike(broken_bike)
      expect { subject.release_bike }.to raise_error 'No bikes available'
   end
 
   it 'Dockingstation should accept a bike broken or not'do 
-      broken_bike = Bike.new 
+      broken_bike = double(:bike)
       broken_bike.broken
-      working_bike = Bike.new 
+      working_bike = double(:bike)
       subject.return_bike(working_bike)
       subject.return_bike(broken_bike)
       expect(subject.release_bike.working?).to eq true
